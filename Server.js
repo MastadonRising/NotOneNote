0c5c2764894948d29f0af723907b370d18319dd1
Luis Sandoval <luissandoval1126@gmail.com>
2020-03-23 21:35:37 -0700

    cant remember all my changes

--- Server.js
+++ Server.js
@@ -1,35 +1,61 @@
-// * The following HTML routes should be created:
-
-
-// * The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
-
-// * The following API routes should be created:
-
-//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 
 //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
 //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
-  const fs = require('fs');
-  const express = require('express');
-  const path = require('path');
-  const app = express();
-  var PORT = process.env.PORT || 8080;
-  
 
-  app.get("/", function(req, res) {
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var writeNote = function(res){
+    fs.readFile(path.join(__dirname, "/db/db.json"), function(err, data) {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        return res.status(200).json(notes);
+    }) 
+};
+app.get("/assets/js/index.js", function(req,res){
+        res.sendFile(path.join(__dirname, "/assets/js/index.js"));
+});
+app.get("/assets/css/styles.css", function(req,res){
+    res.sendFile(path.join(__dirname, "/assets/css/styles.css"));
+});
+
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
-  });
+});
   
-  app.get("/notes", function(req, res) {
+app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
-  });
+});
   
+app.get("/api/notes", function(req, res) {
+  writeNote(res);
+});
+
+app.post("/api/notes", function(req, res) {
+    // let note = JSON.stringify();
+    console.log(req.body);
+    
+    // fs.appendFile(path.join(__dirname, "/db/db.json"),note,'utf8',function(err) {
+    //     if (err) {
+    //       throw err;
+    //     }
+    //     else {
+    //       writeNote(res);
+    //     }
+    // });
+});
 
 
-
-  app.listen(PORT, function() {
+app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
